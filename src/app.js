7d420816f8927992f01eaef5ab18fa680c0430ea
mandeep singh <mandeep14042002@gmail.com>
2024-08-11 01:38:58 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ app.use(express.urlencoded({limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//Routes  import
+import userRouter from "./routes/user.routes.js"
+
+//routes decleration
+app.use("/api/v1/users",userRouter)
+
 
 
 export {app}
