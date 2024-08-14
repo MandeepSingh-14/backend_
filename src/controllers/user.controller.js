308be4b0c534aab0991ad7843442070bb08d427c
mandeep singh <mandeep14042002@gmail.com>
2024-08-15 04:06:53 +0530

    login and logout functionality added with middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,25 @@ import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import e from "cors"
+// hme bhut baar use krna pd skta hai access tokens and refresh tokens so for that we are creating methods gfor this 
+const generateAccessAndRefereshTokens = async(userId) =>{
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
 
 const registerUser = asyncHandler(async (req,res)=>{
     // get user detail from frontend (detail kya kya lunga ye mere m odel pe depend krege)
@@ -31,18 +50,21 @@ const registerUser = asyncHandler(async (req,res)=>{
         throw new ApiError(400,"all fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await   User.findOne({
         $or : [{username}, {email}]
     })
 
     if(existedUser)
     {
-        throw new ApiError(409, "User with username or email ezist")
+        throw new ApiError(409, "User with username or email exist")
     }
 
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath)
     {
@@ -81,6 +103,87 @@ const registerUser = asyncHandler(async (req,res)=>{
 })  
 
 
+const loginUser = asyncHandler(async (req,res) => {
+     // req body -> data
+    // username or email
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie
+
+
+    const {email,username,password} = req.body;
+
+    
+    if (!username && !email) {
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username},{email}]   ///find krega ek value ko jha apko jo value hai vo ya to username ke base pe milegi ya email ke base pe 
+    })
+
+    if(!user)
+        throw new ApiError(404,"User doesnot exist")
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid Credentials")
+    }
+
+    const {accessToken , refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+
+    return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",refreshToken,options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUser,accessToken,refreshToken
+                },
+                "User Logged in successfully"
+            )
+        )
+
+})
+
+
+const logoutUser = asyncHandler(async (req,res) =>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: { 
+                refreshToken : undefined
+            }
+        },
+        {
+            new: true
+        }
+    )   
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+        return res
+        .status(200)
+        .clearCookie("accessToken" ,options)
+        .clearCookie("resfreshToken" ,options)
+        .json(new ApiResponse(200,{},"User succeddfully logged out"))
+    
+})
 export {
     registerUser,
+    loginUser,
+    logoutUser,
 }
