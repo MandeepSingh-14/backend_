71a4d34c6bfee91301d66f704a2b264d601800ec
mandeep singh <mandeep14042002@gmail.com>
2024-08-16 01:24:44 +0530

    Created Subscription model

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,14 +1,14 @@
-// import { subscribe } from "diagnostics_channel";
-// import mongoose ,{Schema} from "mongoose";
+import { subscribe } from "diagnostics_channel";
+import mongoose ,{Schema} from "mongoose";
 
-// const subscriptionSchema = new Schema({
-//     subscriber:{
-//         type: Schema.Types.ObjectId, // one who is subscribing
-//         ref:"User"
-//     },
-//     channel:{
-//             type: Schema.Types.ObjectId, // channel getting subscribed
-//     }
-// },{timestamps: true})
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref:"User"
+    },
+    channel:{
+            type: Schema.Types.ObjectId, // channel getting subscribed
+    }
+},{timestamps: true})
 
-// export const Subscription = mongoose.model("Subscription",subscriptionSchema)
+export const Subscription = mongoose.model("Subscription",subscriptionSchema)
