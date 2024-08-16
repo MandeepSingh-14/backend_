f40a656b179deaa902d1ee63ab4eee666f6980b7
mandeep singh <mandeep14042002@gmail.com>
2024-08-17 03:59:57 +0530

    added comment controller and routes

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { addComment, deleteComment, updateComment } from "../controllers/comment.controller";
+import { verifyJwt } from "../middlewares/auth.middleware.js"
+
+const router =Router()
+
+router.route("/:videoId".post(addComment))
+router.route("/c/:commentId").patch(updateComment).patch(deleteComment)
+
+
+export default router
