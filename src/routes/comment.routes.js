0facb50127d84b66a8654cc22470524238d3938d
mandeep singh <mandeep14042002@gmail.com>
2024-08-19 03:07:03 +0530

    fixed small bugs

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -4,7 +4,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router =Router()
 router.use(verifyJWT)
-router.route("/:videoId".post(addComment))
+router.route("/:videoId").post(addComment)
 router.route("/c/:commentId").patch(updateComment).patch(deleteComment)
 
 
